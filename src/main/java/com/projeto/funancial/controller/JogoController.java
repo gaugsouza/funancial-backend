55fcaca28d54de6f406fe855e18c081818801cb1
calebe <calebe.fazzani.facul@gmail.com>
2019-12-11 21:47:14 -0300

    Fix bug

--- src/main/java/com/projeto/funancial/controller/JogoController.java
+++ src/main/java/com/projeto/funancial/controller/JogoController.java
@@ -1,6 +1,5 @@
 package com.projeto.funancial.controller;
 
-import org.mockito.Mockito;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +14,13 @@ import com.projeto.funancial.transformation.UsuarioTransformation;
 public class JogoController {
 	private UsuarioService usuarioService;
 	private UsuarioTransformation usuarioTransformation;
-	
+
 	public JogoController(UsuarioService usuarioService, UsuarioTransformation usuarioTransformation) {
 		this.usuarioService = usuarioService;
-		this.usuarioTransformation = usuarioTransformation; 
+		this.usuarioTransformation = usuarioTransformation;
 	}
-	
-	public UsuarioCanonical iniciarJogo(UsuarioCanonical usuarioCanonical) { return usuarioCanonical;}
+
+	public UsuarioCanonical iniciarJogo(UsuarioCanonical usuarioCanonical) {
+		return usuarioCanonical;
+	}
 }
